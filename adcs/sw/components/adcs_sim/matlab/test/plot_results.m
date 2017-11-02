8af0f82e3beed5040a24b72828fb1f4b4982aea4
scrice <scrice@uw.edu>
2017-11-02 11:23:15 -0700

    Fixed signal bugs and changed names of plot signals

--- adcs/sw/components/adcs_sim/matlab/test/plot_results.m
+++ adcs/sw/components/adcs_sim/matlab/test/plot_results.m
@@ -5,36 +5,36 @@
 close all
 
 % ----- Extract data ----- %
-states = logsout.getElement('states').Values;
-    sc_quat = states.quaternion; % [ rad ]
-    sc_omega = states.body_rates; % [ rad/s ]
-    sc_pos_eci = states.position; % [ m ]
-    sc_vel_eci = states.velocity; % [ m\s ]
+states_plot = logsout.getElement('states').Values;
+    sc_quat = states_plot.quaternion; % [ rad ]
+    sc_omega = states_plot.body_rates; % [ rad/s ]
+    sc_pos_eci = states_plot.position; % [ m ]
+    sc_vel_eci = states_plot.velocity; % [ m\s ]
     
-disturbances = logsout.getElement('disturbances').Values;
+disturbances_plot = logsout.getElement('disturbances').Values;
 
-orbit_data = logsout.getElement('orbit_data').Values;
+orbit_data_plot = logsout.getElement('orbit_data').Values;
 
 act_meas = logsout.getElement('act_meas').Values;
     
-control = logsout.getElement('control').Values;
-    ctrl_RW         = control.torque;
-    ctrl_dipole     = control.magnetic_dipole;
-sens_meas = logsout.getElement('sens_meas').Values;
+control_plot = logsout.getElement('control').Values;
+    ctrl_RW         = control_plot.torque;
+    ctrl_dipole     = control_plot.magnetic_dipole;
+sens_meas_plot = logsout.getElement('sens_meas').Values;
 
-commands    = logsout.getElement('commands').Values;
+commands_plot = logsout.getElement('commands').Values;
 
-sc_mode = logsout.getElement('sc_mode').Values;
+sc_mode_plot = logsout.getElement('sc_mode').Values;
 
-ctrl_status    = logsout.getElement('ctrl_status').Values;
+ctrl_status_plot    = logsout.getElement('ctrl_status').Values;
 
-sc_in_sun    = logsout.getElement('sc_in_sun').Values;
+sc_in_sun_plot    = logsout.getElement('sc_in_sun').Values;
 
-sc_in_sun_fsw    = logsout.getElement('sc_in_sun_fsw').Values;
+sc_in_sun_fsw_plot    = logsout.getElement('sc_in_sun_fsw').Values;
 
-orbit_tle_fsw    = logsout.getElement('orbit_tle_fsw').Values;
+orbit_tle_fsw_plot    = logsout.getElement('orbit_tle_fsw').Values;
 
-cur_gps_time     = logsout.getElement('cur_gps_time').Values;
+cur_gps_time_plot     = logsout.getElement('cur_gps_time').Values;
 
 % ----- End extract ----- % 
 
@@ -74,8 +74,8 @@ subplot(2,1,2)
 plot(ctrl_RW,'LineWidth',1)
 
 figure(3), hold on
-plot(sc_mode,'LineWidth',1)
-plot(ctrl_status,'LineWidth',1)
+plot(sc_mode_plot,'LineWidth',1)
+plot(ctrl_status_plot,'LineWidth',1)
 
 REKM = 6378.135; % earth radius [km]
 figure(4), hold on
@@ -85,7 +85,7 @@ Y=Y.*REKM;
 Z=Z.*REKM;
 Earth_im = imread('Flat_earth_cds.jpg', 'jpg');
 surf(X, Y, Z,'CData',flip(Earth_im,1),'FaceColor','texturemap','EdgeColor','none');
-plot3(states.position.Data(:,1)./1e3,states.position.Data(:,2)./1e3,states.position.Data(:,3)./1e3,'r','LineWidth',1)
+plot3(states_plot.position.Data(:,1)./1e3,states_plot.position.Data(:,2)./1e3,states_plot.position.Data(:,3)./1e3,'r','LineWidth',1)
 plot3(true_posx(1:t_end),true_posy(1:t_end),true_posz(1:t_end),'b','LineWidth',1)
 %quiver3(0,0,0,avg_sc2sun(1),avg_sc2sun(2),avg_sc2sun(3),'LineWidth',1)
 xlabel('x-direction [km]')
