51638d527d5b45d12fcc1fb77873e50dcc730de1
Taylor Patrick Reynolds <tpr6@uw.edu>
2017-12-06 19:02:58 -0800

    unit tested mag_vec_lib and updated the library accordingly. I think this library was causing a lot of the sim issues that I noticed.

--- adcs/sw/components/adcs_sim/matlab/test/environment-test/disturbances/mag_vector/mag_vec_test_init.m
+++ adcs/sw/components/adcs_sim/matlab/test/environment-test/disturbances/mag_vector/mag_vec_test_init.m
@@ -1,6 +1,10 @@
 %% Magnetic vector unit test init file
 
-% Test 1: 
+% Test 1: Run a static test at an arbitrary lat/long/atl to test against
+% any online B-field computer. I've compared to:
+% http://geomag.nrcan.gc.ca/calc/mfcal-en.php.
+
+% Test 2: 
 
 % UW HuskySat-1, ADCS Subsystem
 %  Last Update: T. Reynolds 12.5.17
@@ -20,8 +24,13 @@ fsw_params.bdot     = init_bdot_controller(fsw_params);
 %% Test 1
 
 % Overrides
-
+% Seattle lat/lon with estimated altitude
+lat     = 47.663888888888889;
+lon     = -122.301111111111111;
+alt     = 500; %km
 % -----
+
+t_end = 10;
 
 % Simulation parameters
 run_time    = num2str(t_end);
@@ -32,11 +41,17 @@ sim(mdl);
 
 % ----- Analyze Results ----- %
 %   extract pos/vel/time data from sim
-dt_aero = logsout.getElement('dt_aero').Values.Data;
-dt_aero_time = logsout.getElement('dt_aero').Values.Time;
+mag_vec_eci_T = logsout.getElement('mag_vec_eci_T').Values.Data;
+mag_vec     = mag_vec_eci_T(end,:);
 
+% Compare this value to whatever online computer gives for the same
+% lat/lon/alt.
+nrm_nT     = 1e9*norm(mag_vec);
+nrm_nT_des  = 53482; % field strength for lat/lon/alt according to reference
 
 % ----- Plot Results ----- %
+diff    = abs(nrm_nT - nrm_nT_des);
+disp(['Test 1: Difference in computed field strength is: ', num2str(diff),'nT']);
 
 %save('workspace-disturbances-test1.mat')
 
