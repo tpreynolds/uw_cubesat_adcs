c606af9be6af891cd56d369d02983fdebe76590f
Taylor <taylorreynolds@Taylors-iMac.local>
2017-12-07 19:33:53 -0800

    fixed many of the running bugs in environment library. Isolated remaining errors to the sensor libraries. Full sim runs nicely if the sensors are commented out, but there is limited use in this, other than a slight uptick in trust of the other libs. Major change is replacing the SGP4 initial condition with a new TLE2ECI library that runs much quicker. Its validity has been checked against the SGP4 lib (its close, not exact).  A better unit test needed (maybe something from vallado) to verify new library.

--- adcs/sw/components/adcs_sim/matlab/test/environment-test/disturbances/grav_dist/grav_dist_test_init.m
+++ adcs/sw/components/adcs_sim/matlab/test/environment-test/disturbances/grav_dist/grav_dist_test_init.m
@@ -1,6 +1,8 @@
 %% Gravity gradient unit test init file
 
-% Test 1: 
+% Test 1: Plug in a lat/lon/alt and observe the magnitude of the gravity
+% field. Static test. Compared with online calculator at 
+% https://planetcalc.com/1758/.
 
 % UW HuskySat-1, ADCS Subsystem
 %  Last Update: T. Reynolds 12.5.17
@@ -15,13 +17,14 @@ fsw_params = init_fsw_params();
 [sim_params,fsw_params] = init_sim_params(fsw_params);
 fsw_params.bdot     = init_bdot_controller(fsw_params);
 
-
-
 %% Test 1
 
 % Overrides
-
+lat     = 45;
+lon     = -122;
+alt     = 500e3;
 % -----
+t_end   = 1;
 
 % Simulation parameters
 run_time    = num2str(t_end);
@@ -32,10 +35,12 @@ sim(mdl);
 
 % ----- Analyze Results ----- %
 %   extract pos/vel/time data from sim
-dt_aero = logsout.getElement('dt_aero').Values.Data;
-dt_aero_time = logsout.getElement('dt_aero').Values.Time;
+grav_eci_ms2 = logsout.getElement('grav_eci_ms2').Values.Data;
+DT_gg = logsout.getElement('DT_gg').Values.Data;
 
-
+g   = norm(grav_eci_ms2(end,:));
+disp(['Acceleration due to gravity is: ',num2str(g),'m/s2 at ',...
+        num2str(alt/1e3),'km altitude and ',num2str(lat),' deg latitude']);
 % ----- Plot Results ----- %
 
 %save('workspace-disturbances-test1.mat')
