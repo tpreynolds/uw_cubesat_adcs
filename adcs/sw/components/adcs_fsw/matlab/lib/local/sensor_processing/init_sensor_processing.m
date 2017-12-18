47f2342a8324771f7be7ba45cdaecd3bcd0265f9
Taylor Patrick Reynolds <tpr6@uw.edu>
2017-12-18 18:26:22 -0500

    finished updates on conversions between TLEs and ECIs coordinates. Updated GPS propagation in flight software. Cannot get rid of errors in the magnetic field library. The test file works, but the sim still does not run properly. More testing needed.

--- adcs/sw/components/adcs_fsw/matlab/lib/local/sensor_processing/init_sensor_processing.m
+++ adcs/sw/components/adcs_fsw/matlab/lib/local/sensor_processing/init_sensor_processing.m
@@ -28,8 +28,7 @@ sensor_processing.sunsensor.rate    = (1/10); % Hz - sensor read rate
 % ----- GPS Sensor ----- %
 sensor_processing.gps.ic.time = [fsw_params.environment.sgp4.gps_sec_init ...
                                  fsw_params.environment.sgp4.gps_week_init]';
-sensor_processing.gps.ic.pos    = zeros(3,1);
-sensor_processing.gps.ic.vel    = zeros(3,1);
+[sensor_processing.gps.ic.pos,sensor_processing.gps.ic.vel]    = TLE2ECI(fsw_params.environment.sgp4.orbit_tle);
 sensor_processing.gps.sample_time_s     = 1;
 sensor_processing.gps.rate  = 1; % Hz - sensor read rate
 % ---------------------- %
--- adcs/sw/components/adcs_sim/matlab/lib/local/sensor/gps/init_gps.m
+++ adcs/sw/components/adcs_sim/matlab/lib/local/sensor/gps/init_gps.m
@@ -1,5 +1,13 @@
 function gps = init_gps()
+% Initialize the GPS sensor library block.
+% HuskySat-1, ADCS Subsystem
+% Last Update: T. Reynolds 12.18.17
 
 gps.sample_time_s = 1;
 gps.num_sats    = 5;
-gps.max_sec = 604800;
+gps.max_sec     = 604800;
+gps.period      = 5400; % roughly one orbit
+gps.pct_on      = 1; % turn it on for up to 1% of one orbit
+gps.delay_s     = 0; % takes 15s to get a lock
+
+end
