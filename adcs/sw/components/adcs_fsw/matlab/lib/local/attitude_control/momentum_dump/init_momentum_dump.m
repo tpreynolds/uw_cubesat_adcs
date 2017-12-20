4bdbe88872cdc860d6099e0218fee066fafb977e
Emma Hansen <emmah0@uw.edu>
2017-12-20 12:55:33 -0500

    added parameter for reference angular momentum

--- adcs/sw/components/adcs_fsw/matlab/lib/local/attitude_control/momentum_dump/init_momentum_dump.m
+++ adcs/sw/components/adcs_fsw/matlab/lib/local/attitude_control/momentum_dump/init_momentum_dump.m
@@ -2,7 +2,7 @@ function p_dump = init_momentum_dump( )
 % ----------------------------------------------------------------------- %
 % UW HuskySat-1, ADCS Team
 %   Load the parameters for the momentum (p) unloading controller here. 
-% Last Edited: T. Reynolds 9.29.17
+% Last Edited: E. Hansen 12.20.17
 % ----------------------------------------------------------------------- %
 
 % Initial Conditions
@@ -12,6 +12,7 @@ p_dump.ic.rt_dipole_Am2     = zeros(3,1);
 p_dump.ic.rt_PPT_on         = 0;
 p_dump.ic.rt_B_meas_valid   = 0;
 p_dump.ic.delay             = zeros(3,1);
+p_dump.ic.ang_mom_ref       = 2000;
 
 % Sample time
 p_dump.sample_time_s    = 1/5; % sample at 5 Hz
