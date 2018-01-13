35bc1efc1af35081f819cb4178473a431a484f44
Emma Hansen <emmah0@uw.edu>
2018-01-13 13:13:36 -0800

    Still troubleshooting - encountering problems with altitude

--- adcs/sw/components/adcs_fsw/matlab/test/momentum_unload_test/init_momentum_dump.m
+++ adcs/sw/components/adcs_fsw/matlab/test/momentum_unload_test/init_momentum_dump.m
@@ -12,14 +12,14 @@ p_dump.ic.rt_dipole_Am2     = zeros(3,1);
 p_dump.ic.rt_PPT_on         = 0;
 p_dump.ic.rt_B_meas_valid   = 0;
 p_dump.ic.delay             = zeros(3,1);
-p_dump.ic.ang_mom_ref       = fsw_params.actuators.reaction_wheel.inertia_matrix*[max(fsw_params.bus.RW_RPM_thresh)*fsw_params.convert.RPM_2_RADPS;...
-                                                     max(fsw_params.bus.RW_RPM_thresh)*fsw_params.convert.RPM_2_RADPS;...
-                                                     max(fsw_params.bus.RW_RPM_thresh)*fsw_params.convert.RPM_2_RADPS]; 
+p_dump.ic.ang_mom_ref       = fsw_params.actuators.reaction_wheel.inertia_matrix*[min(fsw_params.bus.RW_RPM_thresh)*fsw_params.convert.RPM_2_RADPS;...
+                                                     min(fsw_params.bus.RW_RPM_thresh)*fsw_params.convert.RPM_2_RADPS;...
+                                                     min(fsw_params.bus.RW_RPM_thresh)*fsw_params.convert.RPM_2_RADPS]; 
 
 % Sample time
 p_dump.sample_time_s    = 1/5; % sample at 5 Hz
 
 % Momentum unloading controller gains
-p_dump.gain_k     = 2*10^(-4);
+p_dump.gain_k     = 2*10^(-3);
 
 
