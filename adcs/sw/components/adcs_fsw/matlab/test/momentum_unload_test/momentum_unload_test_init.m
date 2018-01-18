741d0f6b90a9b948534568521e413b7e280ee681
Emma Hansen <emmah0@uw.edu>
2018-01-18 10:28:19 -0800

    updating to most recent changes

--- adcs/sw/components/adcs_fsw/matlab/test/momentum_unload_test/momentum_unload_test_init.m
+++ adcs/sw/components/adcs_fsw/matlab/test/momentum_unload_test/momentum_unload_test_init.m
@@ -11,9 +11,9 @@ clear variables; close all; clc
 addpath(genpath('../../../matlab/')) % adds the fsw libs
 addpath(genpath('../../../../adcs_sim/matlab/')) % add the sim libs
 
-run_test    = 3;
+run_test    = 1;
 
-t_end   = 15;
+t_end   = 820;
 %% Test 1
 if run_test == 1
 
@@ -25,192 +25,6 @@ fsw_params = init_fsw_params();
 [sim_params,fsw_params] = init_sim_params(fsw_params);
 
 % Overrides
-J  = fsw_params.bus.inertia;
-sim_params.dynamics.ic.rate_init = zeros(3,1);
-
-% Choose damping ratio and natural frequency
-z   = 1; % Critically damped
-wn  = 0.02*2*pi; % Small natural frequency
-
-fsw_params.control.pd_controller.p_gain  = -wn^2.*J;
-fsw_params.control.pd_controller.d_gain  = -2*wn*z.*J;
-
-% fsw_params.control.pd_controller.p_gain = -kp;
-% fsw_params.control.pd_controller.d_gain = -kd;
-%temp    = randn(4,1);
-%sim_params.dynamics.ic.quat_init    = temp./norm(temp);
-sim_params.dynamics.ic.quat_init    = [1; 0; 0; 0];
-
-eul_angle   = deg2rad(10);
-eul_axis    = [1; 0; 0];
-sim_params.dynamics.ic.quat_ini = [cos(eul_angle/2); sin(eul_angle/2)*eul_axis];
-fsw_params.bus.quat_commanded   = [cos(eul_angle/2); sin(eul_angle/2)*eul_axis];
-% -----
-
-% Simulation parameters
-run_time    = num2str(t_end);
-mdl         = 'momentum_unload_test';
-load_system(mdl);
-set_param(mdl, 'StopTime', run_time);
-sim(mdl);
-
-% % ----- Analyze Results ----- %
-% 
-% quat    = logsout.getElement('<quaternion>').Values.Data;
-% omega   = logsout.getElement('<body_rates>').Values.Data;
-% cmdu       = logsout.getElement('cmd_torque').Values.Data;
-realu       = logsout.getElement('torque').Values.Data;
-% real_time   = logsout.getElement('torque').Values.Time;
-% cmdRPM     = squeeze(logsout.getElement('cmd_rpm').Values.Data);
-% realRPM     = logsout.getElement('real_RPM').Values.Data;
-% 
-% q_d     = fsw_params.bus.quat_commanded;
-% diff    = zeros(1,length(tout));
-% for i = 1:length(tout)
-%     q_diff  = quatmultiply(quatconj(q_d'),quat(i,:));
-%     diff(i) = norm( q_diff(2:4) ) ;
-% end
-% 
-% 
-% % ----- End Analysis ----- %
-% 
-% figure(1)
-% subplot(2,1,1)
-% plot(tout,quat)
-% title('Quaternion','FontSize',15)
-% subplot(2,1,2)
-% plot(tout,omega)
-% title('Angular Velocity [rad/s]','FontSize',15)
-% xlabel('Time [s]','FontSize',12)
-% 
-% figure(2)
-% subplot(2,2,1)
-% plot(real_time,cmdu)
-% title('Commanded Torque [Nm]','FontSize',15)
-% subplot(2,2,2)
-% plot(tout,cmdRPM)
-% title('Commanded RW RPM','FontSize',15)
-% xlabel('Time [s]','FontSize',12)
-% subplot(2,2,3)
-% plot(real_time,realu)
-% title('Actual Torque [Nm]','FontSize',15)
-% subplot(2,2,4)
-% plot(tout,realRPM)
-% title('Actual RW RPM','FontSize',15)
-% xlabel('Time [s]','FontSize',12)
-% 
-% figure(3), hold on
-% plot(tout,diff,'LineWidth',1)
-% plot(tout,0.02*ones(1,length(tout)),'k--')
-% %plot([ts ts],[0 1],'k--')
-% xlabel('Time [s]','FontSize',12)
-% 
-% %save('workspace-test-NAME.mat')
-
-elseif run_test == 2
-%% Test 2
-
-% Load bus stub definitions
-load('bus_definitions.mat')
-
-% Load parameters for both flight software and simulation
-fsw_params = init_fsw_params();
-[sim_params,fsw_params] = init_sim_params(fsw_params);
-
-% Overrides
-
-% -----
-
-% Simulation parameters
-run_time    = num2str(t_end);
-mdl         = 'pd_control_test';
-load_system(mdl);
-set_param(mdl, 'StopTime', run_time);
-sim(mdl);
-
-% ----- Analyze Results ----- %
-quat    = logsout.getElement('<quaternion>').Values.Data;
-omega   = logsout.getElement('<body_rates>').Values.Data;
-cmdu       = logsout.getElement('cmd_torque').Values.Data;
-realu       = logsout.getElement('torque').Values.Data;
-real_time   = logsout.getElement('torque').Values.Time;
-cmdRPM     = squeeze(logsout.getElement('cmd_rpm').Values.Data);
-realRPM     = logsout.getElement('real_RPM').Values.Data;
-
-q_d     = fsw_params.bus.quat_id;
-diff    = zeros(1,length(tout));
-for i = 1:length(tout)
-    q_diff  = quatmultiply(quatconj(q_d'),quat(i,:));
-    diff(i) = norm( q_diff(2:4) ) ;
-end
-
-
-% ----- End Analysis ----- %
-
-figure(1)
-subplot(2,1,1)
-plot(tout,quat)
-title('Quaternion','FontSize',15)
-subplot(2,1,2)
-plot(tout,omega)
-title('Angular Velocity [rad/s]','FontSize',15)
-xlabel('Time [s]','FontSize',12)
-
-figure(2)
-subplot(2,2,1)
-plot(real_time,cmdu)
-title('Commanded Torque [Nm]','FontSize',15)
-subplot(2,2,2)
-plot(tout,cmdRPM)
-title('Commanded RW RPM','FontSize',15)
-xlabel('Time [s]','FontSize',12)
-subplot(2,2,3)
-plot(real_time,realu)
-title('Actual Torque [Nm]','FontSize',15)
-subplot(2,2,4)
-plot(tout,realRPM)
-title('Actual RW RPM','FontSize',15)
-xlabel('Time [s]','FontSize',12)
-
-figure(3), hold on
-plot(tout,diff,'LineWidth',1)
-plot(tout,0.02*ones(1,length(tout)),'k--')
-%plot([ts ts],[0 1],'k--')
-xlabel('Time [s]','FontSize',12)
-
-%save('workspace-test-NAME.mat')
-
-elseif run_test==3
-%% Test 3
-
-% Load bus stub definitions
-load('bus_definitions.mat')
-
-% Load parameters for both flight software and simulation
-fsw_params = init_fsw_params();
-[sim_params,fsw_params] = init_sim_params(fsw_params);
-
-% Overrides
-J  = fsw_params.bus.inertia;
-%sim_params.dynamics.ic.rate_init = zeros(3,1);
-
-% Choose damping ratio and natural frequency
-z   = 1; % Critically damped
-wn  = 0.02*2*pi; % Small natural frequency
-
-fsw_params.control.pd_controller.p_gain  = -wn^2.*J;
-fsw_params.control.pd_controller.d_gain  = -2*wn*z.*J;
-
-% fsw_params.control.pd_controller.p_gain = -kp;
-% fsw_params.control.pd_controller.d_gain = -kd;
-%temp    = randn(4,1);
-%sim_params.dynamics.ic.quat_init    = temp./norm(temp);
-sim_params.dynamics.ic.quat_init    = [1; 0; 0; 0];
-
-eul_angle   = deg2rad(10);
-eul_axis    = [1; 0; 0];
-sim_params.dynamics.ic.quat_ini = [cos(eul_angle/2); sin(eul_angle/2)*eul_axis];
-fsw_params.bus.quat_commanded   = [cos(eul_angle/2); sin(eul_angle/2)*eul_axis];
 % -----
 % Simulation parameters
 run_time    = num2str(t_end);
@@ -224,7 +38,7 @@ sldebug(mdl);
 % quat    = logsout.getElement('<quaternion>').Values.Data;
 % omega   = logsout.getElement('<body_rates>').Values.Data;
 % cmdu       = logsout.getElement('cmd_torque').Values.Data;
-realu       = logsout.getElement('torque').Values.Data;
+% realu       = logsout.getElement('torque').Values.Data;
 % mag_dip     = logsout.getElement('magnetic_dipole').Values.Data;
 % real_time   = logsout.getElement('torque').Values.Time;
 % cmdRPM     = squeeze(logsout.getElement('cmd_rpm').Values.Data);
