37ec0faf24109e1d849302ebf76d69f85bacee6d
Taylor <taylorreynolds@Taylors-iMac.local>
2018-02-02 13:07:14 -0800

    updated coord test. Major bug in ECi/ECEF conversion, errors are 300km and 0.7km/s. May be causing the results we see in the sim.

--- adcs/sw/components/adcs_fsw/matlab/test/coord_rotations_time/coord_rot_time_test_init.m
+++ adcs/sw/components/adcs_fsw/matlab/test/coord_rotations_time/coord_rot_time_test_init.m
@@ -1,11 +1,15 @@
 %% Target Generation unit test init file
 % UW HuskySat-1, ADCS Subsystem
 %
-% Test 1: Coordinate rotation test. Runs the simulink file
-% coord_rotations_test.slx using two examples from Vallado 4ed. Compares
+% Test 1: Coordinate rotation test for TEME to ECI. Runs the simulink file
+% coord_rotations_test.slx using example from Vallado 4ed, pp 233-234. Compares
 % the outputs against the values given in the book.
 %
-% Test 2: Time conversion test. 
+% Test 2: Coordinate rotation test for ECEF to ECI. Runs the simulink file
+% coord_rotations_test.slx using example 3-15 from Vallado 4ed, pp 230-231. 
+% Compares the outputs against the values given in the book.
+%
+% Test 3: Time conversion test.
 %
 %  Last Update: T. Reynolds 2.3.18
 %% Assuming sim_init.m has been run
@@ -14,12 +18,15 @@ set(0,'defaulttextinterpreter','latex');
 
 % Constants
 JD_2_cent   = 1/(36525);
+JD_2_sec    = 86400;
+sec_2_JD    = 1/86400;
 sec_2_cent  = 1/(86400*36525);
 dut1        = 0.204241;
 ut1_2_tt    = 32.184 + 32 + dut1;
+JD_J2000    = 2451545.0;
 
 % Choose test
-run_test    = 1;
+run_test    = 2;
 
 %% Test 1
 
@@ -47,55 +54,52 @@ sim(mdl);
 rECI_comp    = logsout.getElement('rECI_comp').Values.Data;
 vECI_comp    = logsout.getElement('vECI_comp').Values.Data;
 
-
-
+rERR    = norm(rECI - rECI_comp(1,:)');
+vERR    = norm(vECI - vECI_comp(1,:)');
 % ----- End Analysis ----- %
+
+disp(['Position error is: ',num2str(1e3*rERR),' m | ', ...
+        'Velocity error is: ',num2str(1e3*vERR),' m/s'])
 
 
 %save('workspace-test-NAME.mat')
 
 elseif run_test == 2
 %% Test 2
+% Date-time is 2004-04-06-07-51-27.946047 UT1
 
-% Load bus stub definitions
-load('bus_definitions.mat')
-
-% Load parameters for both flight software and simulation
-fsw_params = init_fsw_params();
-[sim_params,fsw_params] = init_sim_params(fsw_params);
-
-% Overrides
-t_end       = 5400;
-sc_mode     = 32;
-sc2gs_eci_unit  = zeros(3,1);
-% -----
+% Inputs
+t_end  = 1;
+dut1    = -0.4399619;
+s_frac  = 0.946047;                         % Fractional part of UT1 seconds
+JD      = 2453101.827396 + sec_2_JD*s_frac; % Converted date-time w/o fractional seconds online.
+JD_ut1_cent     = JD_2_cent*(JD - JD_J2000);
+JD_tt_cent      = 0.0426236319;             % From the example.
+% initial vectors
+rECI    = [ -1033.4793830; 7901.2952754; 6380.3565958 ];
+vECI    = [ -3.225636520; -2.872451450; 5.531924446 ];
+% desired vectors
+rECEF   = [ 5102.508958; 6123.01152; 6378.136928 ];
+vECEF   = [ -4.74322016; 0.79053650; 5.53375528 ];
+% ----- end inputs ----- %
 
 % Simulation parameters
 run_time    = num2str(t_end);
-mdl         = 'target_generation_test2';
+mdl         = 'coord_rotations_test';
 load_system(mdl);
 set_param(mdl, 'StopTime', run_time);
 sim(mdl);
 
 % ----- Analyze Results ----- %
-quat        = logsout.getElement('quat').Values.Data;
-omega       = logsout.getElement('body_rates').Values.Data;
-quat_cmd    = logsout.getElement('quat_cmd').Values.Data;
-omega_cmd   = logsout.getElement('omega_cmd_radps').Values.Data;
+rECEF_comp    = logsout.getElement('rECEF_comp').Values.Data;
+vECEF_comp    = logsout.getElement('vECEF_comp').Values.Data;
 
-
+rERR    = norm(rECEF - rECEF_comp(1,:)');
+vERR    = norm(vECEF - vECEF_comp(1,:)');
 % ----- End Analysis ----- %
 
-figure(1)
-subplot(2,1,1), hold on
-plot(tout,quat)
-plot(tout,quat_cmd,'--')
-title('Quaternion','FontSize',15)
-subplot(2,1,2), hold on
-plot(tout,omega)
-plot(tout,omega_cmd,'--')
-title('Angular Velocity [rad/s]','FontSize',15)
-xlabel('Time [s]','FontSize',12)
+disp(['Position error is: ',num2str(1e3*rERR),' m | ', ...
+        'Velocity error is: ',num2str(1e3*vERR),' m/s'])
 
 %save('workspace-test-NAME.mat')
 end
