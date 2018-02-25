762084fecdde4af3232b7974f2506e279bf8ba9e
Taylor <taylorreynolds@Taylors-iMac.local>
2018-02-24 16:09:33 -0800

    finished command processing test

--- adcs/sw/components/adcs_fsw/matlab/test/command-processing/command_processing_test_init.m
+++ adcs/sw/components/adcs_fsw/matlab/test/command-processing/command_processing_test_init.m
@@ -1,7 +1,8 @@
 %% Command Processing unit test init file
 % UW HuskySat-1, ADCS Team
 %
-% Test 1: Basic test with different input signals
+% Test 1: Basic test with different input signals. The plots show the
+% responses in various axes.
 %
 % Primary Test: T. Reynolds 2.23.18
 %% Assuming sim_init.m has been run
@@ -35,26 +36,26 @@ tout        = tout(2:end);
 
 figure(1)
 subplot(2,1,1)
-plot(tout,cmd_DV(:,1),'LineWidth',1)
-ylabel('DV')
+plot(tout,cmd_DV(:,3),'LineWidth',1)
+ylabel('DV'); title('Commanded dipole vs. Digital Value','FontSize',15)
 subplot(2,1,2)
-plot(tout,dipole_in(:,1),'LineWidth',1)
+plot(tout,dipole_in(:,3),'LineWidth',1)
 xlabel('Time [s]'); ylabel('Dipole')
-title('Commanded dipole vs. Digital Value','FontSize',15)
+
 
 figure(2)
 subplot(2,1,1)
 plot(tout,cmd_RPM(:,1),'LineWidth',1)
-ylabel('RPM')
+ylabel('RPM'); title('Commanded RPM vs. Torque','FontSize',15) 
 subplot(2,1,2)
 plot(tout,torque_in(:,1),'LineWidth',1)
-xlabel('Time [s]'); ylabel('Torque')
-title('Commanded RPM vs. Torque','FontSize',15) 
+xlabel('Time [s]'); ylabel('Torque [Nm]')
+
 
 %save('workspace-test-NAME.mat')
 
 elseif run_test == 2
-
+% no test 2 for now
 
 %save('workspace-test-NAME.mat')
 
