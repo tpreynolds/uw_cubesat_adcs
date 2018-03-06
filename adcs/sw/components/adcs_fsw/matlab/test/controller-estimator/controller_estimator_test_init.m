35e63e988bd27f2a927764b604b7a91717bc2538
Taylor <taylorreynolds@Taylors-iMac.local>
2018-03-06 14:07:18 -0800

    updated EKF unit test. Not passed yet.

--- adcs/sw/components/adcs_fsw/matlab/test/controller-estimator/controller_estimator_test_init.m
+++ adcs/sw/components/adcs_fsw/matlab/test/controller-estimator/controller_estimator_test_init.m
@@ -1,30 +1,37 @@
-% run sim init before starting this script
+%% EKF Estimator Test
+% Test 1:
+%   Provide static inertial vectors and random initial estimator states.
+%   The controller should steer the estimated state the (static) commanded
+%   state. Required estimator convergencence in under 60s.
+
+% UW HuskySat-1, ADCS Subsystem
+%  Primary: M. Hudoba de Badyn -- x.x.xx
+%   Secondary: T. Reynolds -- 3.6.18
+
+% Note: Assumes sim_init.m has been run
 clc; close all;
 set(0,'defaulttextinterpreter','latex');
-cd /media/sexymathemagician/LaCie1/uw_cubesat_adcs/adcs/sw/components/adcs_fsw/matlab/test/controller-estimator/
-addpath(genpath(pwd))
+%% Test 1
 
 % seed the random initial conditions so you get the same stuff everytime
 rng(155);
 
-figdir = strcat(pwd,'/figs/');
-datadir = strcat(pwd,'/data/');
-
-% attitude conditions
-quat_cmd = -[-0.1 0.2 0.3 0.4]';
+% Attitude conditions
+quat_cmd = randn(4,1);%-[-0.1; 0.2; 0.3; 0.4];
 quat_cmd = quat_cmd/norm(quat_cmd);
-sim_params.dynamics.ic.quat_init = [1 0 0 0];
-fsw_params.estimation.ic.quat_est_init = rand(4,1);
+sim_params.dynamics.ic.quat_init        = [1 0 0 0]';
+fsw_params.estimation.ic.quat_est_init  = rand(4,1);
 
-fsw_params.estimation.ic.rt_w_body_radps = 0*[0.1 -0.05 -0.03]';
-sim_params.dynamics.ic.rate_init = 0*[0.1 -0.05 -0.03]';
+% Angular velocity
+fsw_params.estimation.ic.rt_w_body_radps    = 0*[0.01 -0.05 -0.03]';
+sim_params.dynamics.ic.rate_init            = 0*[0.1 -0.05 -0.03]';
 
-% make the noise larger (set 1 otherwise)
-var_mult_mt = 10000;
-var_mult_sun = 10000;
-var_mult_gyro = 100;
+% make the noise larger (set 1 for flight values)
+var_mult_mt     = 1;
+var_mult_sun    = 1;
+var_mult_gyro   = 1;
 
-% measurement vectors
+% Inertial measurement vectors
 mag_vec_init = [4 1 -8]';
 mag_vec_init = mag_vec_init/norm(mag_vec_init);
 
@@ -33,19 +40,21 @@ sun_vec_init = sun_vec_init/norm(sun_vec_init);
 
 % body frame
 A = quatToMatrix(quat_cmd);
-fsw_params.estimation.ic.rt_mag_eci_est = A*mag_vec_init;
-fsw_params.estimation.ic.rt_sun_eci_est = A*sun_vec_init;
+fsw_params.estimation.ic.rt_mag_eci_est = mag_vec_init;%.*randn(3,1);
+fsw_params.estimation.ic.rt_sun_eci_est = sun_vec_init;%.*randn(3,1);
 
 % reference frame
-fsw_params.estimation.ic.rt_mag_body = mag_vec_init;
-fsw_params.estimation.ic.rt_sun_body = sun_vec_init;
+fsw_params.estimation.ic.rt_mag_body = A'*mag_vec_init;%.*randn(3,1);
+fsw_params.estimation.ic.rt_sun_body = A'*sun_vec_init;%.*randn(3,1);
 
+% Run the test
 run_time    = '200';
-mdl         = 'controller_estimator_minimal';
+mdl         = 'controller_estimator_test_static';
 load_system(mdl);
 set_param(mdl, 'StopTime', run_time);
 sim(mdl);
 
+% Plot results
 h1 = figure;
 plot(omega_hat.Time, omega_hat.Data(:,1),'r--'), hold on
 plot(omega_hat.Time, omega_hat.Data(:,2),'b--')
@@ -67,8 +76,6 @@ plot(quat_true.Time, quat_cmd(1)*ones(size(quat_true.Time)), 'k','linewidth',2)
 plot(quat_true.Time, quat_cmd(2)*ones(size(quat_true.Time)), 'k','linewidth',2)
 plot(quat_true.Time, quat_cmd(3)*ones(size(quat_true.Time)), 'k','linewidth',2)
 plot(quat_true.Time, quat_cmd(4)*ones(size(quat_true.Time)), 'k','linewidth',2)
-
-
 
 h3 = figure;
 plot(bias_hat)
