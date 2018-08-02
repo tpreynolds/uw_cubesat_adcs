4f4199ec29f1e372928b5eeb649af808f186919e
Taylor Patrick Reynolds <tpr6@uw.edu>
2018-08-02 13:20:33 -0400

    redid LS magnetic function, finished fixed step stuff.

--- adcs/sw/components/adcs_fsw/matlab/include/init_fsw_constants.m
+++ adcs/sw/components/adcs_fsw/matlab/include/init_fsw_constants.m
@@ -36,14 +36,14 @@ constants.convert.rev2rad       = 2*pi;
 constants.convert.rad2rev       = 1/(2*pi);
 
 % Mag field approx
-x   = load('mag_field_approx.mat');
-constants.mag.b0    = x.x(1:3);
-constants.mag.b1    = x.x(4:6);
-constants.mag.b2    = x.x(7:9);
-constants.mag.b3    = x.x(10:12);
-constants.mag.b4    = x.x(13:15);
-constants.mag.b5    = x.x(16:18);
-constants.mag.b6    = x.x(19:21);
+load('mag_field_approx.mat');
+constants.mag.b0    = X(:,1);
+constants.mag.b1    = X(:,2);
+constants.mag.b2    = X(:,3);
+constants.mag.b3    = X(:,4);
+constants.mag.b4    = X(:,5);
+constants.mag.b5    = X(:,6);
+constants.mag.b6    = X(:,7);
 
 % Misc.
 constants.twopi     = 2*pi;
--- adcs/sw/components/adcs_sim/matlab/tools/LS_mag_init.m
+++ adcs/sw/components/adcs_sim/matlab/tools/LS_mag_init.m
@@ -4,6 +4,7 @@
 
 % Set the orbit TLE
 TLE                         = fsw_params.bus.orbit_tle;
+% TLE(2)  = TLE(2) - fsw_params.constants.time.JD_J2000;
 
 % Compute orbital frequency
 Om  = TLE(9) * fsw_params.constants.convert.revpday_2_radpmin * ...
@@ -23,37 +24,44 @@ set_param(mdl,'StopTime', num2str(run_time));
 sim(mdl);
 
 % Basis functions
-f   = @(t)([ ones(3,1); cos(Om*t) * ones(3,1); sin(Om*t) * ones(3,1); ...
-                cos(2*Om*t) * ones(3,1); sin(2*Om*t) * ones(3,1); ...
-                cos(3*Om*t) * ones(3,1); sin(3*Om*t) * ones(3,1) ]);
+f   = @(t)([ 1; cos(Om*t); sin(Om*t); ...
+                cos(2*Om*t); sin(2*Om*t); ...
+                cos(3*Om*t); sin(3*Om*t) ]);
 
 % Set matrices for LS fit
 n   = length(tout);         % Number of data points
 m   = length(f(0));         % Number of basis functions
 A   = zeros(n,m);
-b   = zeros(3*n,1);
+bx  = zeros(n,1);
+by  = zeros(n,1);
+bz  = zeros(n,1);
 
-for k = 1:n
-    tk                  = JD_ut1_J2000(k);
-    fk                  = f(tk);
-    rows                = 3*(k-1)+1:3*k;
-    b(rows,1)           = mag_vec_eci_T(k,:)';
-    for l = 1:(m/3)
-        cols         = 3*(l-1)+1:3*l;
-        A(rows,cols) = diag(fk(cols));
-    end
+% Solve for coefficients
+for row = 1:n
+   tk   = fsw_params.constants.time.day2sec * (JD_ut1_J2000(row) - TLE(2));
+   fk   = f(tk);
+   bx(row,1) = mag_vec_eci_T(row,1);
+   by(row,1) = mag_vec_eci_T(row,2);
+   bz(row,1) = mag_vec_eci_T(row,3);
+   for col = 1:m
+      A(row,col) = fk(col); 
+   end
 end
 
-X   = pinv(A) * b;
+Xx  = pinv(A) * bx;
+Xy  = pinv(A) * by;
+Xz  = pinv(A) * bz;
+
+X   = [ Xx'; Xy'; Xz' ];
 
 % Get vector coefficients
-b0  = X(1:3);
-b1  = X(4:6);
-b2  = X(7:9);
-b3  = X(10:12);
-b4  = X(13:15);
-b5  = X(16:18);
-b6  = X(19:21);
+b0  = X(:,1);
+b1  = X(:,2);
+b2  = X(:,3);
+b3  = X(:,4);
+b4  = X(:,5);
+b5  = X(:,6);
+b6  = X(:,7);
 
 F   = @(t)( b0 + b1*cos(Om*t) + b2*sin(Om*t) + ...
                 b3 * cos(2*Om*t) + b4 * sin(2*Om*t) + ...
@@ -64,7 +72,7 @@ B_approx    = zeros(3*n,1);
 B_err       = zeros(n,1);
 
 for k = 1:n
-    tk                          = JD_ut1_J2000(k);
+    tk                          = fsw_params.constants.time.day2sec * (JD_ut1_J2000(k) - TLE(2));
     B_approx(3*(k-1)+1:3*k,1)   = F(tk);
     B_true                      = mag_vec_eci_T(k,:);
     B_err(k)    = norm(B_approx(3*(k-1)+1:3*k,1) - B_true,2);
@@ -77,13 +85,16 @@ mean_err    = mean(B_err);
 fprintf('The mean error is: %2.3e.\n',mean_err)
 
 % Plot results
+close all
 figure(1), hold on, grid on
 plot(tout,mag_vec_eci_T,'lineWidth',1)
-plot(tout,B_approx,'LineWidth',1)
-xlabel('Simulation time [s]')
-title('Comparison of calculated B-vectors')
+plot(tout,B_approx,'--','LineWidth',1)
+xlabel('Simulation time [s]','FontSize',14)
+ylabel('B [T]','FontSize',14)
+title('WMM 2015 versus LS Magnetic Field','FontSize',16)
 
-
+% Save coefficients
+save('../../adcs_fsw/matlab/tools/mag_field_approx.mat','X')
 
 
 
