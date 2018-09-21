8f11208d74e2d45872a2420f3a8789b898765acc
Taylor Patrick Reynolds <tpr6@uw.edu>
2018-09-20 22:19:38 -0400

    brought in the TLE generation and updated to be more accurate/flexible. Updated the initialization for SGP4 so that we can define an orbit and have it spit out the TLE that is used in simulation.

--- adcs/sw/components/adcs_fsw/matlab/lib/local/env_estimation/orbit_estimation/sgp4/init_sgp4.m
+++ adcs/sw/components/adcs_fsw/matlab/lib/local/env_estimation/orbit_estimation/sgp4/init_sgp4.m
@@ -7,18 +7,28 @@ function sgp4   = init_sgp4(fsw_params)
 %   Init file for sgp4 library.
 %
 %   T. Reynolds -- 2.10.18
+%   updated: T. Reynolds -- 9.20.18 -- added TLE_gen functionality
 % ----------------------------------------------------------------------- %
 
+    % Conversions
     DEG2RAD         = fsw_params.constants.convert.deg2rad;
     REVpD2RADpM     = fsw_params.constants.convert.revpday_2_radpmin;
     
-     sgp4.tle_filename = 'SWISSCUBE.tle';
-%     sgp4.tle_filename = 'QUAKESAT.tle';
+    % Define our orbit
+    YMDHMS  = [ 2019; 1; 1; 0; 0; 0 ];
+    INC     = 54.6146;  % inclination
+    RAAN    = 247.4627; % right ascension of ascending node
+    ECC     = 0006703; % keep this w/o decimals and 7 digits
+    AOP     = 130.5360; % arg of perigee
+    MNA     = 325.0288; % mean anomaly
+    MNM     = 15.72125391; % mean motion
+    
+    % Generate TLE (saved as text file in /adcs_sim/matlab/include/TLEs/
+    sgp4.orbit_tle = TLE_gen(YMDHMS, INC, RAAN, ECC, AOP, MNA, MNM);
 
-    [sgp4.orbit_tle,sgp4.JD_epoch_days] = get_tle(sgp4.tle_filename);
-
-    sgp4.y       = sgp4.orbit_tle(1);
-    sgp4.jd_epoch_days   = sgp4.orbit_tle(2);
+    % Add individual quantities to SGP4 struct (correct units)
+    sgp4.year    = sgp4.orbit_tle(1);
+    sgp4.JD_UTC_epoch_J2000 = sgp4.orbit_tle(2);
     sgp4.B_star  = sgp4.orbit_tle(3);
     sgp4.INC     = sgp4.orbit_tle(4)*DEG2RAD;       % [rad]
     sgp4.RAAN    = sgp4.orbit_tle(5)*DEG2RAD;       % [rad]
@@ -27,11 +37,7 @@ function sgp4   = init_sgp4(fsw_params)
     sgp4.MNA     = sgp4.orbit_tle(8)*DEG2RAD;       % [rad]
     sgp4.MNM     = sgp4.orbit_tle(9)*REVpD2RADpM;   % [rad/min]
 
-    % DON'T CHANGE THE TLE W/O CHANGING THE GPS START TIME
-    % SWISSCUBE and QUAKESAT have the same epoch
-    % sec_init from GPS time + account for leap seconds
-    sgp4.gps_sec_init   = 154800 + 37.0 - 19.0; % last two are for leap sec
-    sgp4.gps_week_init  = 2021; 
-    sgp4.gps_time       = [sgp4.gps_sec_init; sgp4.gps_week_init];
+    % Compute initial GPS time
+    sgp4.gps_time   = JDUTC_2_GPS(sgp4.JD_UTC_epoch_J2000,fsw_params);
 
 end
---
+++ adcs/sw/components/adcs_sim/matlab/tools/TLE_gen.m
@@ -0,0 +1,99 @@
+% UW HuskySat-1, ADCS Subsystem
+%   S. Rice 5/21/18
+%   updated: T. Reynolds
+%
+% Script for generating our TLE format. Will need to be updated to get a
+% full TLE.
+% S.Rice 5/22/18
+%
+% Assumes sim_init.m has been run to set paths
+
+function [orbit_tle,JD_UTC_epoch] = TLE_gen(YMDHMS, INC, RAAN, ECC, AOP, MNA, MNM, varargin)
+
+% Run checks
+if( nargin < 7 )
+    error('TLE_gen: not enough inputs')
+end
+if( nargin > 7 )
+    MNM_dot = varargin{1};
+    % pad this with zeros like ECC when needed
+    if( nargin > 8 )
+        if( nargin == 8 )
+            error('TLE_gen: Must give both B_star and B_star_ex')
+        else
+            B_star = varargin{2};
+            B_star_ex = varargin{3};
+        end
+    end
+else
+    % Assumed values from SWISSCUBE
+    B_star      = 32923;
+    B_star_ex   = -4;
+    sMNM_dot     = strcat('+.','00000',num2str(145));
+end
+
+if( numel(YMDHMS) < 6 )
+    error('TLE_gen: Not enough entries in YMDHMS; must be 6')
+end
+
+year     = YMDHMS(1);
+fullyear = year;
+if( year > 2000 )
+    year = year - 2000;
+else
+    year = year - 1900;
+end
+month   = YMDHMS(2);
+day     = YMDHMS(3);
+hour    = YMDHMS(4);
+min     = YMDHMS(5);
+sec     = YMDHMS(6);
+
+% Find the decimal day of year
+Lmonth  = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+if( mod(year,4) == 0 )
+    Lmonth(2) = 29;
+end
+day_dec = 0.0;
+for m = 1:month-1
+    day_dec = day_dec + Lmonth(m);
+end
+day_dec = day_dec + day + hour/24 + min/1440 + sec/86400;
+
+% Julian date at Jan 1, epoch_year, 00:00:00
+JD_begin_of_year    = 367*fullyear - floor((7/4)*(fullyear + floor(10/12))) + ...
+                        floor(275/9) + 1721013.5;
+JD_UTC_epoch_J2000  = day_dec + JD_begin_of_year - 2451545;
+
+% Reformat the eccentricity1
+nz  = 7 - floor(log10(ECC)) - 1;
+switch nz
+    case 0
+        sECC = num2str(ECC,'%7.0f');
+    case 1
+        sECC = strcat('0',num2str(ECC,'%6.0f'));
+    case 2
+        sECC = strcat('00',num2str(ECC,'%5.0f'));
+    case 3 
+        sECC = strcat('000',num2str(ECC,'%4.0f'));
+    case 4 
+        sECC = strcat('0000',num2str(ECC,'%3.0f'));
+    case 5
+        sECC = strcat('00000',num2str(ECC,'%2.0f'));
+    case 6
+        sECC = strcat('000000',num2str(ECC,'%1.0f'));
+    case 7
+        sECC = '0000000';
+end
+
+% Output a text file with this TLE
+fID = fopen('include/TLEs/ourTLE.tle','w');
+fprintf(fID,'1 00000U 18001Z   %02.0f%012.8f %s  00000-0 %+5.0f%2.0f 0  0017\n',year,day_dec,sMNM_dot,B_star,B_star_ex);
+fprintf(fID,'2 00000  %7.4f %08.4f %s %08.4f %08.4f %10.8f563537',INC,RAAN,sECC,AOP,MNA,MNM);
+fclose(fID);
+
+% Output a TLE in the format used for sim
+B_star      = B_star*10^(B_star_ex); 
+ECC         = ECC/1e7;
+orbit_tle   = [year, JD_UTC_epoch_J2000, B_star, INC, RAAN, ECC, AOP, MNA, MNM]';
+end
