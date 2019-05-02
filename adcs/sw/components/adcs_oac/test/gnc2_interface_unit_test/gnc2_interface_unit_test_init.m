03e892fa8299d410782daf04fbff50c6c3b8862a
Taylor Patrick Reynolds <tpr6@uw.edu>
2019-05-01 17:15:59 -0700

    work on soac interface. Need to write the interpolation/integration function to generate the state/control references

--- adcs/sw/components/adcs_oac/test/gnc2_interface_unit_test/gnc2_interface_unit_test_init.m
+++ adcs/sw/components/adcs_oac/test/gnc2_interface_unit_test/gnc2_interface_unit_test_init.m
@@ -9,11 +9,16 @@ T_max = 1e-3;
 
 rng(2) % for repeatability of random IC
 
+% sc mode
+sc_mode = 1;
+
 % initial conditions
 ax  = [1;1;1]./norm([1;1;1]);
 ang = 30*randn;
 quat_in  = [ cosd(ang/2); sind(ang/2).*ax ];
 omega_in = 0.01.*randn(3,1);
+Om_in = 0.10471975511966 * [ 1000; 1000; 1000 ]; % rad/s
+hw_in = fsw_params.bus.inertia * Om_in;
 
 % final conditions
 quat_cmd  = [ 1.0; 0.0; 0.0; 0.0 ];
@@ -26,13 +31,13 @@ sI_unit  = [ 1.0; 0.0; 0.0 ];
 GPS_epoch = sim_params.environment.sgp4.gps_time;
 
 % Load sim and set run time
-run_time    = 10;
+run_time    = 25;%soac_params.s_max;
 mdl         = 'gnc2_interface_unit_test';
 load_system(mdl);
 set_param(mdl,'StopTime', num2str(run_time));
 
 % run test case
-sim(mdl);
+% sim(mdl);
 
 %% analyze results
 
