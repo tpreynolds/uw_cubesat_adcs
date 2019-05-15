12b030bc94729ff0ee08024ef3253fea68691bd0
Taylor Patrick Reynolds <tpr6@uw.edu>
2019-05-15 16:59:21 -0700

    successful interface design and unit test!

--- adcs/sw/components/adcs_oac/test/gnc2_interface_unit_test/gnc2_interface_unit_test_init.m
+++ adcs/sw/components/adcs_oac/test/gnc2_interface_unit_test/gnc2_interface_unit_test_init.m
@@ -13,13 +13,13 @@ J     = [ 0.0338    -4.884e-05 -7.393e-05;
 
 rng(2) % for repeatability of random IC
 
-sc_mode = 1;
+sc_mode = 33;
 
 % initial conditions
 ax  = [1;1;1]./norm([1;1;1]);
-ang = 60*randn;
+ang = 60;
 quat_in  = [ cosd(ang/2); sind(ang/2).*ax ];
-omega_in  = 0.01.*randn(3,1);
+omega_in  = zeros(3,1);%0.01.*randn(3,1);
 Om0 = 0.10471975511966 * [ 1000; 1000; 1000 ]; % rad/s
 hw_in = Jw * Om0;
 
@@ -43,4 +43,39 @@ set_param(mdl,'StopTime', num2str(run_time));
 % sim(mdl);
 
 %% analyze results
+
+u_opt = command_torque;
+x_opt = command_state;
+
+xf = x_opt(end,:);
+qf = xf(1:4);
+wf = xf(5:7);
+hwf = xf(8:10);
+
+% Plot
+close all
+figure
+subplot(3,1,1), hold on, grid on
+% plot(T,X(:,1:4),'LineWidth',1)
+plot(tout,x_opt(:,1:4),'LineWidth',1)
+xlabel('Time [s]')
+title('Attitude Quaternion')
+subplot(3,1,2), hold on, grid on
+plot(tout,x_opt(:,5:7),'LineWidth',1)
+% plot(OAC.t,OAC.inertia\xopt(5:7,:),'ko','MarkerSize',3)
+plot([0 tout(end)],[OAC.w_max OAC.w_max],'r--','LineWidth',1)
+plot([0 tout(end)],[-OAC.w_max -OAC.w_max],'r--','LineWidth',1)
+xlabel('Time [s]')
+title('Angular Velocity')
+subplot(3,1,3), hold on, grid on
+% plot(T,X(:,8:10),'LineWidth',1)
+plot(tout,x_opt(:,8:10),'LineWidth',1)
+xlabel('Time [s]')
+title('Wheel Momentum')
+
+figure, hold on, grid on
+plot(tout,u_opt,'LineWidth',1)
+xlabel('Time [s]')
+title('Control Signal')
+
 
