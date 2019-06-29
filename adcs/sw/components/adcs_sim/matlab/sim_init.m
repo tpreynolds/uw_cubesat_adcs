7f6c7770dc5cd98d6ceaade38a40e89cee466945
Taylor Patrick Reynolds <tpr6@uw.edu>
2019-06-28 17:58:38 -0700

    working on the ICD for the payload

--- adcs/sw/components/adcs_sim/matlab/sim_init.m
+++ adcs/sw/components/adcs_sim/matlab/sim_init.m
@@ -1,4 +1,4 @@
-% ----------------------------------------------------------------------- %
+cl% ----------------------------------------------------------------------- %
 %SIM_INIT   Initialize the RAIN-SAT Simulator
 %
 % Initializes all simulation parameters for 'adcs_sim_main.slx'. This is
